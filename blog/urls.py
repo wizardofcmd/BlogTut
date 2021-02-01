d9f60d3a7914f64854a13264ed228143f84e7f1f
wizardofcmd <sorinnechiforora@gmail.com>
2021-02-01 12:58:31 +0000

    Added urls & views

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name='blog-home'),
+    path('about/', views.about, name='blog-about'),
 ]
